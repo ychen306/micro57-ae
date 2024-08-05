b5d001d10de29fad0db0fa89cfebbfe9e9fe7399
tom <ychen306@mit.edu>
2024-08-05 19:06:34 -0400

    add rle build

---
+++ build-rle.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+if ! (echo 'int main() {return 0;}' | $1/bin/clang -x c - -o /dev/null)
+then
+    echo "Usage: ./run-tsvc.sh <path to llvm-15's build directory> [optionally, number of cores to use]"
+    exit 1
+fi
+
+abspath() {
+    cd "$(dirname "$1")"
+    printf "%s/%s\n" "$(pwd)" "$(basename "$1")"
+    cd "$OLDPWD"
+}
+
+LLVM_PATH=$(abspath $1)
+
+JOBS=$2
+if ! [[ $2 =~ ^[0-9]+$ ]]
+then
+    JOBS=4
+fi
+
+echo "Compiling with $JOBS cores"
+
+git clone https://github.com/ychen306/pssa pssa-rle
+cd pssa-rle
+git checkout micro57-ae-rle
+
+# Build OR Tools
+curl -L https://github.com/google/or-tools/archive/refs/tags/v9.7.tar.gz -o or-tools.tar.gz
+tar -xvf or-tools.tar.gz
+OR_TOOLS_BUILD_PATH="$(abspath or-tools-9.7)/install_make"
+cd or-tools-9.7
+echo Building or-tools at $OR_TOOLS_BUILD_PATH
+make cpp JOBS=$JOBS
+cd -
+
+mkdir build
+echo Building rle at $(abspath build)$
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release\
+  -DCMAKE_PREFIX_PATH="$LLVM_PATH;$OR_TOOLS_BUILD_PATH"\
+  -DCMAKE_CXX_FLAGS="-fpermissive"\
+  ../
+make -j$JOBS
+cd ../
